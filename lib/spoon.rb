15048b1e1ba72db847f251cce73dbff3ed732d79
Matthew Scharley <matt@scharley.me>
2010-08-27 21:55:55 +1000

    Added an initial attempt at a CreateProcess definition, as well as some basic infrastructure to handle using both calls.

--- lib/spoon.rb
+++ lib/spoon.rb
@@ -17,6 +17,41 @@ module Spoon
   rescue FFI::NotFoundError
   end
   
+  # BOOL WINAPI CreateProcess(
+  #   __in_opt     LPCTSTR lpApplicationName,
+  #   __inout_opt  LPTSTR lpCommandLine,
+  #   __in_opt     LPSECURITY_ATTRIBUTES lpProcessAttributes,
+  #   __in_opt     LPSECURITY_ATTRIBUTES lpThreadAttributes,
+  #   __in         BOOL bInheritHandles,
+  #   __in         DWORD dwCreationFlags,
+  #   __in_opt     LPVOID lpEnvironment,
+  #   __in_opt     LPCTSTR lpCurrentDirectory,
+  #   __in         LPSTARTUPINFO lpStartupInfo,
+  #   __out        LPPROCESS_INFORMATION lpProcessInformation
+  # );
+  
+  begin
+    ffi_lib 'kernel32'
+    ffi_convention :stdcall
+    
+    attach_function :_create_process, :CreateProcessW, [:buffer_in, :pointer, :pointer, :pointer, :int, :int, :pointer, :buffer_in, :pointer, :pointer], :int
+    
+    class SecurityAttributes < FFI::Struct
+      layout :length, :int,                   # DWORD
+             :security_descriptor, :pointer,  # LPVOID
+             :inherit_handler, :int           # BOOL
+    end
+    
+    class StartupInfo < FFI:Struct
+      
+    end
+    
+    class ProcessInformation < FFI:Struct
+      
+    end
+  rescue FFI::NotFoundError
+  end
+  
   def self.have_spawn?
     begin
       self.method(:_posix_spawn)
@@ -25,23 +60,55 @@ module Spoon
     end
   end
   
+  def self.have_create_process?
+    begin
+      self.method(:_create_process)
+    rescue NameError
+      false
+    end
+  end
+  
   def self.supported?
-    have_spawn?
+    have_spawn? or have_create_process?
   end
   
   def self.spawn(*args)
+    if have_spawn?
+      spawn_spawn(*args)
+    else
+      spawn_cp(*args)
+    end
+  end
+
+  def self.spawnp(*args)
+    if have_spawn?
+      spawnp_spawn(*args)
+    else
+      spawnp_cp(*args)
+    end
+  end
+  
+  private
+  
+  def self.spawn_spawn(*args)
     spawn_args = _prepare_spawn_args(args)
     _posix_spawn(*spawn_args)
     spawn_args[0].read_int
-end
-
-  def self.spawnp(*args)
+  end
+  
+  def self.spawn_cp(*args)
+    
+  end
+  
+  def self.spawnp_spawn(*args)
     spawn_args = _prepare_spawn_args(args)
     _posix_spawnp(*spawn_args)
     spawn_args[0].read_int
   end
   
-  private
+  def self.spawnp_cp(*args)
+    
+  end
   
   def self._prepare_spawn_args(args)
     pid_ptr = FFI::MemoryPointer.new(:pid_t, 1)
